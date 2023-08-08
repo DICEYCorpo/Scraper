0291abb294914cfeba2234cafb4ce486174415e9
Hasan Mohammod Tanzir <104096448+DICEYCorpo@users.noreply.github.com>
2023-08-08 13:45:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+import openai
 
 load_dotenv()
-search_url = "https://api.bing.microsoft.com/v7.0/search"
-search_term = "Women entrepreneurs grants"
+with open("prompts","r") as f:
+    prompts = f.read()
 
-headers = {"Ocp-Apim-Subscription-Key": os.getenv("SUBKEY")}
-params = {"q": search_term, "textDecorations": True, "textFormat": "HTML", "count": 25}
-response = requests.get(search_url, headers=headers, params=params)
-response.raise_for_status()
-search_results = response.json()
-
-pages = (search_results['webPages'])
-results = pages['value']
-for result in results:
-    print(result['url'])
+openai_api = os.getenv("OPENAIAPI")
+openai.api_key = openai_api
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": prompts}
+    ]
+)
+print(response['choices'][0]['message']['content'])
+# search_url = "https://api.bing.microsoft.com/v7.0/search"
+# search_term = "Women entrepreneurs grants"
+#
+# headers = {"Ocp-Apim-Subscription-Key": os.getenv("SUBKEY")}
+# params = {"q": search_term, "textDecorations": True, "textFormat": "HTML", "count": 25}
+# response = requests.get(search_url, headers=headers, params=params)
+# response.raise_for_status()
+# search_results = response.json()
+#
+# pages = (search_results['webPages'])
+# results = pages['value']
+# for result in results:
+#     print(result['url'])
 
 
 
