b384a52146d4e02655354aaa697c7ce52010f2fd
Hasan Mohammod Tanzir <104096448+DICEYCorpo@users.noreply.github.com>
2023-08-08 16:17:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import openai
-
 load_dotenv()
+
+
 with open("prompts","r") as f:
     prompts = f.read()
+
 
 openai_api = os.getenv("OPENAIAPI")
 openai.api_key = openai_api
@@ -16,20 +18,38 @@ response = openai.ChatCompletion.create(
         {"role": "user", "content": prompts}
     ]
 )
-print(response['choices'][0]['message']['content'])
-# search_url = "https://api.bing.microsoft.com/v7.0/search"
-# search_term = "Women entrepreneurs grants"
-#
-# headers = {"Ocp-Apim-Subscription-Key": os.getenv("SUBKEY")}
-# params = {"q": search_term, "textDecorations": True, "textFormat": "HTML", "count": 25}
-# response = requests.get(search_url, headers=headers, params=params)
-# response.raise_for_status()
-# search_results = response.json()
-#
-# pages = (search_results['webPages'])
-# results = pages['value']
-# for result in results:
-#     print(result['url'])
+
+gpt_response = response['choices'][0]['message']['content']
+
+with open("response.txt", "w") as tfile:
+    tfile.write(gpt_response)
+with open('response.txt', 'r') as tempfile:
+    lines = tempfile.readlines()
+# Remove the "- " from each line
+cleaned_lines = [line.replace('- ', '') for line in lines]
+# Write the modified lines back to the file
+with open('cleaned_grants.txt', 'w') as cfile:
+    cfile.writelines(cleaned_lines)
+
+
+
+with open("cleaned_grants.txt", "r") as file:
+    temp = file.readlines()
+    for lines in temp:
+        print(lines)
+        search_url = "https://api.bing.microsoft.com/v7.0/search"
+        search_term = lines
+
+        headers = {"Ocp-Apim-Subscription-Key": os.getenv("SUBKEY")}
+        params = {"q": search_term, "textDecorations": True, "textFormat": "HTML", "count": 10}
+        response = requests.get(search_url, headers=headers, params=params)
+        response.raise_for_status()
+        search_results = response.json()
+        pages = (search_results['webPages'])
+        results = pages['value']
+        for result in results:
+            print(result['url'])
+            print()
 
 
 
